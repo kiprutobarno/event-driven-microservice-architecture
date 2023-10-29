a47223b5effd789c99f17a4415e65776e43eb95c
ywalakamar <barxwells@gmail.com>
2023-10-29 17:43:57 +0300

    chore: microservice project setup

---
+++ stock-service/src/test/java/com/tuts/stockservice/StockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuts.stockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
